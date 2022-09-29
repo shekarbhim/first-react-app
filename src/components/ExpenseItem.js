a6e84b5204d6ed8ec0178b2137612d389e41ffdf
shekarbhim <shekar.bhimreddy@gmail.com>
2022-09-29 17:07:33 +0530

    add components and props

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item_description">
+        <h2>{props.title}</h2>
+        <div className="expense-item_price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+}
+export default ExpenseItem;
